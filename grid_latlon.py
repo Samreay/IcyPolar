6fe43446eaa745ef8f78cf0361e5766df38da223
Christopher Jordan <christopherjordan87@gmail.com>
2017-04-29 21:49:06 +1000

    Added all-in-one script, nasa_csv2raster_txt.py. Fixed common.py, and made grid_latlon.py more of a diagnostic (plotting).

--- grid_latlon.py
+++ grid_latlon.py
@@ -4,20 +4,40 @@ import sys
 
 import numpy as np
 from scipy.interpolate import griddata
+import matplotlib.pyplot as plt
+
+import common
+
+
+def read_NASA_csv(filename, header_length=10):
+    with open(x, 'r') as f:
+        lng, lat, elev = [], [], []
+        for i, line in enumerate(f):
+            if i < header_length:
+                continue
+
+            line = line.replace(", ", ',')
+            cells = line.split(',')
+
+            lng.append(float(cells[1]))
+            lat.append(float(cells[2]))
+            elev.append(float(cells[3]))
+    return lng, lat, elev
 
 
 # Read CSV file
 lng, lat, elev = np.loadtxt(sys.argv[1], delimiter=',', unpack=True)
 
+x, y, z = common.LLA_to_ECEF(lng, lat, elev)
+
 # Resolution is determined by the average separation between points.
 # Use the smallest separation between longitude and latitudes for square pixels.
-resolution = min([np.diff(sorted(lng)).mean(), np.diff(sorted(lat)).mean()])
+resolution = min([np.diff(sorted(x)).mean(), np.diff(sorted(y)).mean()])
 
-grid_x, grid_y = np.meshgrid(np.arange(lng.min(), lng.max(), resolution),
-                             np.arange(lat.min(), lat.max(), resolution))
-cartesian_grid = griddata(np.vstack((lng, lat)).T, elev,
+grid_x, grid_y = np.meshgrid(np.arange(x.min(), x.max(), resolution),
+                             np.arange(y.min(), y.max(), resolution))
+cartesian_grid = griddata(np.vstack((x, y)).T, z,
                           (grid_x, grid_y), method="linear", fill_value=-99)
 
-# import matplotlib.pyplot as plt
-# plt.imshow(cartesian_grid)
-# plt.show()
+plt.imshow(np.log(cartesian_grid))
+plt.show()
